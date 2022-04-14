c923f2cbe578c9bb5b33bcca26b595014f558bf7
NO NAME 07 <103588608+No-Name-07@users.noreply.github.com>
2022-04-14 09:18:53 +0700

    Add files via upload

---
+++ SCRIPT-TERMINAL.sh
@@ -0,0 +1,366 @@
+#!/usr/bin/bash
+a='\033[34;1m' #biru
+b='\033[32;1m' #ijo
+c='\033[35;1m' #purple
+d='\033[36;1m' #cyan
+f='\033[31;1m' #merah
+g='\033[37;1m' #purple
+h='\033[33;1m'
+
+e=echo
+clear
+$e $b""
+figlet BIO RECOX
+$e $b"••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+$e $b"•"                             $d"Y"$b"O"$d"U"$b"T"$d"U"$b"B"$d"E"$b" : BIO RECOX"          $a"WA : 082264049558"
+$e $b"•               INSTAGRAM : BIO RECOX"
+$e $b"••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+$e
+$e $c"	            SCRIPT TERMINALL "
+$e $h"{"$g"1"$h"}" $b"BRUTERFORCE FACEBOOK"
+$e $h"{"$g"2"$h"}" $b"BRUTERFORCE INSTAGRAM"
+$e $h"{"$g"3"$h"}" $b"MAIREL PHISING"
+$e $h"{"$g"4"$h"}" $b"LOKMED AUTO EXPLOIT"
+$e $h"{"$g"5"$h"}" $b"AUTO EXPLOTI CMS"
+$e $h"{"$g"6"$h"}" $b"JOOMLAODAY"
+$e $h"{"$g"7"$h"}" $b"SPYDERBOT"
+$e $h"{"$g"8"$h"}" $b"PSQLI PRO"
+$e $h"{"$g"9"$h"}" $b"MASS WP FILE MANAGER"
+$e $h"{"$g"10"$h"}" $b"XMLRPC BRUTERFORCE ATTACK"
+$e $h"{"$g"11"$h"}" $b"SAYDOG PRAMEWORK"
+$e $h"{"$g"12"$h"}" $b"XSSTRIKE"
+$e $h"{"$g"13"$h"}" $b"MASKPHISH"
+$e $h"{"$g"14"$h"}" $b"BLAZY"
+$e $h"{"$g"15"$h"}" $b"D-TECT"
+$e $h"{"$g"16"$h"}" $b"SPAM CALL"
+$e $h"{"$g"17"$h"}" $b"ADMIN FINDER"
+$e $h"{"$g"18"$h"}" $b"ARP SPOOFING DETECTOR"
+$e $h"{"$g"19"$h"}" $b"SQLI BALITBANG"
+$e $h"{"$g"20"$h"}" $b"FACEBOOK ACCOUNT BRUTEFORCE"
+$e $h"{"$g"21"$h"}" $b"KNOCKPY"
+$e $h"{"$g"22"$h"}" $b"CAM HACKERS"
+$e $h"{"$g"23"$h"}" $b"SEEKER"
+$e $h"{"$g"24"$h"}" $b"RECONSPIDER"
+echo
+echo $b"MASUKAN PILIHAN LU ANJING"
+read -p"BIO > " pil
+
+																									if [ $pil = 1 ] 
+																then
+																	clear
+																										sleep 1
+																	git clone https://github.com/HostIngsS/IngsBF
+													cd IngsBF
+																		python2 Bruteings.py
+														fi
+
+									if [ $pil = 2 ] 
+						then
+clear
+sleep 1
+											git clone https://github.com/TermuxHackz/instagram-bruteforcer
+cd instagram-bruteforcer
+																	python3 IBF.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+											sleep 1
+																	git clone https://www.github.com/BiZken/PhishMailer.git
+cd PhishMailer
+chmod  + x  PhishMailer . py
+python3 PhishMailer.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+sleep 1
+																				git clone https://github.com/fauzanw/Lokmed-AutoExploit
+cd Lokmed-AutoExploit
+php lokmed-autoexploit.php
+fi
+
+if [ $pil = 5 ]
+then
+clear
+sleep 1
+													git clone https://github.com/mohitsahunitrr/ICG-AutoExploiterBoT
+cd ICG-AutoExploiterBoT
+AutoExploit.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+sleep 1
+git clone https://github.com/KTN1990/joomla-com_xcloner-upload_shell
+cd joomla-com_xcloner-upload_shell
+python2 joom0day.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+sleep 1
+git clone https://github.com/M0sterHxck/SpyderBot
+cd SpyderBot
+python2 Spyder.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+sleep 1
+git clone https://github.com/Agressiv1njector/psqli-pro
+cd psqli-pro
+bash psqli.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pip install colorama
+pip2 install colorama
+git clone https://github.com/kiddenta911/wp-file-manager-0day-mass
+cd wp-file-manager-0day-mass
+python2 scanner.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+sleep 1
+git clone https://github.com/TheRipperJhon/WpBrute-Priv8
+cd WpBrute-Priv8
+python2 wpBrute.py
+fi
+if [ $pil = 11 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install python2
+pkg install git
+pkg install clang
+pkg install libxml2
+pkg install libxslt
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install bs4
+pip2 install bs4
+pip install colorama
+pip2 install colorama
+pip install Cython
+pip2 install Cython
+pip install yagmail
+pio2 install yagmail
+git clone https://github.com/saydog-official/saydog-framework
+cd saydog-framework
+chmod +x*
+./setup
+fi
+
+if [ $pil = 12 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pip install requests
+pip2 install requests
+pip2 install colorama
+pip install colorama
+git clone https://github.com/s0md3v/XSStrike
+cd XSStrike
+python3 xsstrike.py
+fi
+
+if [ $pil = 13]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pkg install curl
+pip install requests
+pip2 install requests
+pip2 install colorama
+pip install colorama
+git clone https://github.com/jaykali/maskphish
+cd maskpilphish
+bash maskphish.sh
+fi
+if [ $pil = 14 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install python2
+pkg install git
+pkg install clang
+pkg install libxml2
+pkg install libxslt
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install bs4
+pip2 install bs4
+pip install colorama
+pip2 install colorama
+pip install Cython
+pip2 install Cython
+git clone https://github.com/s0md3v/Blazy
+cd Blazy
+chmod +x *
+pip2 install -r requirements.txt
+python2 blazy.py
+fi
+
+
+if [ $pil = 15 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip2 install colorama
+pip install colorama
+git clone https://github.com/shawarkhanethicalhacker/D-TECT.git
+cd D-TECT
+python2 d-tect.py
+fi
+
+
+if [ $pil = 16 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pip install requests
+pip2 install requests
+git clone https://github.com/DARK-02/Spam_Scw
+cd Spam_Scw
+python3 run.py
+fi
+
+
+if [ $pil = 17 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/AdheBolo/AdminFinder
+cd AdminFinder
+python2 AdminFinder.py
+fi
+
+
+if [ $pil = 18 ]
+then
+clear
+sleep 1
+pip install scapy
+pip install argparse
+https://github.com/LasCC/ARP-Detector
+cd ARP-Detector
+python3 main.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python
+pkg install python2
+pkg install curl
+pip install requests
+pip2 install requests
+pip2 install colorama
+pip install colorama
+git clone https://github.com/404rgr/mass-sqli-balitbang
+cd mass-sqli-balitbang
+php balit.php 
+fi
+
+
+if [ $pil = 20 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/th3unkn0n/facebash-termux
+cd facebash-termux
+chmod +x *
+sh install.sh
+cd facebash-termux
+./facebash.sh
+fi
+
+if [ $pil = 21 ]
+then
+clear
+sleep 1
+apt-get update
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/guelfoweb/knock.git
+cd knock
+pip3 install -r requirements.txt
+python3 knockpy.py
+fi
+
+if [ $pil = 22 ]
+then
+clear
+sleep 1
+apt-get install python3
+apt-get install git
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+cd Cam-Hackers
+pip3 install requests
+pip3 install colorama
+cd Cam-Hackers
+python3 cam-hackers.py
+fi
+if [ $pil = 23 ]
+then
+clear
+sleep 1
+git clone https://github.com/thewhiteh4t/seeker.git
+cd seeker
+apt update
+apt install python3 python3-pip php
+pip3 install requests
+python3 seeker.py
+fi
+if [ $pil = 24 ]
+then
+clear
+sleep 1
+git clone https://github.com/bhavsec/reconspider.git
+cd reconspider
+sudo python3 setup.py install
+fi
